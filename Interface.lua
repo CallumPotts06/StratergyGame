f9104f014092f57bd6305292f24a3e07ac149f0f
Callum Potts <callum.andrew.potts@gmail.com>
2025-05-29 19:48:11 +0000

    Interface_1.0

---
+++ Interface.lua
@@ -0,0 +1,33 @@
+
+interface = {}
+
+--[[
+
+Interface object allows The Main.lua to quickly create simple GUI objects
+An interface object could be a text box or an image, and is specified on
+instantiation of the object.
+
+]]--
+
+function interface.New(initName,initImg,initColour,initText,initTextColour,initBorder,initBorderColour,initPos,initSize)
+    local newGUI = {}
+
+    -- ADMIN --
+    newGUI.Name = initName
+
+    -- STYLE --
+    newGUI.Image = initImg
+    newGUI.Colour = initColour
+    
+    newGUI.Text = initText
+    newGUI.TextColour = initTextColour
+
+    newGUI.Border = initBorder
+    newGUI.BorderColour = initBorderColour
+
+    -- STATS --
+    newGUI.Position = initPos
+    newGUI.Size = initSize
+end
+
+return interface
