b91a17567d56af5eed526c0e1d201b8b609a81eb
Callum Potts <callum.andrew.potts@gmail.com>
2025-05-31 18:37:57 +0100

    Network_1.1.13

--- Main.lua
+++ Main.lua
@@ -84,12 +84,14 @@ function love.update(dt)
 
     if Network.Hosting then
         local events = Network.InboundEvents()
-        if events[1] == "connected" then
-            print("A Peer Has Connected!")
-        elseif event[1] == "disconnected" then
-            print("A Peer Has Disconnected")
-        elseif event[1] == "received" then
-            print("Recieved: "..event[2])
+        if events then
+            if events[1] == "connected" then
+                print("A Peer Has Connected!")
+            elseif event[1] == "disconnected" then
+                print("A Peer Has Disconnected")
+            elseif event[1] == "received" then
+                print("Recieved: "..event[2])
+            end
         end
     end
 
