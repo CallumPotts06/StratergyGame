19387e059f239652e5a79d5f1abb3bb06dabaa5b
Callum Potts <callum.andrew.potts@gmail.com>
2025-05-31 19:18:21 +0100

    Network_1.1.20

--- Network.lua
+++ Network.lua
@@ -37,12 +37,13 @@ function Network.InboundEvents()
 end
 
 function Network.SendMessage(msg)
-    event = host:service(100)
+    --[[event = host:service(100)
     if event then
         print("Event is True")
         peer = event.peer
         peer:send(msg)
-    end
+    end]]
+    peer:send(msg)
 end
 
 return Network
